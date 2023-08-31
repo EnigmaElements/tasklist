bd99d40a4bcfd98116b5a582e1c82bac70cbd801
EnigmaElements <143720775+EnigmaElements@users.noreply.github.com>
2023-08-31 16:42:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Constants declared for input button and task list area
+const taskInput = document.querySelector("#newtask input");
+const taskSection = document.querySelector('.tasks');
+
+// Listener for the Enter key. Used to add a new task.
+taskInput.addEventListener("keyup", (e) => {
+    if (e.key == "Enter") {
+        createTask();
+    }
+});
+
+// The onclick event for the 'Add' button.
+document.querySelector('#push').onclick = function () {
+    createTask();
+};
+
+// The function that creates a task
+function createTask() {
+    if (taskInput.value.length == 0) {
+        alert("The task field is blank. Enter a task name and try again.");
+    } else {
+        // This block inserts HTML that creates each task into the task area div element
+        taskSection.innerHTML +=
+            `<div class="task">
+                <label id="taskname">
+                    <input onclick="updateTask(this)" type="checkbox" id="check-task">
+                    <p>${document.querySelector('#newtask input').value}</p>
+                </label>
+                <div class="delete">
+                    <i class="uil uil-trash"></i>
+                </div>
+            </div>`;
+
+        // Reset the input field after adding a task
+        taskInput.value = '';
+
+        taskSection.offsetHeight >= 300
+            ? taskSection.classList.add("overflow")
+            : taskSection.classList.remove("overflow");
+
+        // Set up event listener for delete buttons using event delegation
+        taskSection.querySelectorAll(".delete").forEach(deleteButton => {
+            deleteButton.onclick = function () {
+                this.parentNode.remove();
+            };
+        });
+    }
+}
+
+function updateTask(task) {
+    let taskItem = task.parentElement.lastElementChild;
+    if (task.checked) {
+        taskItem.classList.add("checked");
+    } else {
+        taskItem.classList.remove("checked");
+    }
+}
